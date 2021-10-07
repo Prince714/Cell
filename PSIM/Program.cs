a55cb7f79f61bfbc21dd56628a17220caae99dad
prince <A00229731@mycambrian.ca>
2021-10-07 13:21:42 -0400

    Lab 3 cell done

---
+++ PSIM/Program.cs
@@ -0,0 +1,41 @@
+﻿using Psim.Particles;
+using Psim.Geometry2D;
+using Psim.Modelcomponents
+
+using System;
+
+namespace Psim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cell c = new Cell(10, 10);
+            for (int i = 0; i < 30; i++)
+            {
+
+                if (i >= 20)
+                {
+
+                    c.AddIncPhonon(new Phonon(1));
+                }
+                else
+                {
+                    c.AddPhonon(new Phonon(1));
+                }
+            }
+
+
+
+
+
+
+            Console.WriteLine(p);
+            s.HandlePhonon(p);
+            s2.HandlePhonon(p);
+            Console.WriteLine(p);
+
+
+        }
+    }
+}
